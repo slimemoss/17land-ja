9e2b81caa851904a12f5850274ed7d83bb51d461
slimemoss <common.tken@gmail.com>
2025-06-18 15:33:30 +0000

    サイト上でのフィルター時に、ループに入るバグ修正

--- contents/main.ts
+++ contents/main.ts
@@ -1,3 +1,4 @@
+import { debounce } from "lodash"
 import type { PlasmoCSConfig } from "plasmo"
 import { isChageTable, updateScrollTable } from "~script/card-data-page";
 import { observePageTransition } from "~script/observe-page-transition";
@@ -17,23 +18,43 @@ export const stop_observe = (obs: MutationObserver) => {
   obs.disconnect()
 }
 
+const debouncedChangeTableHandler = debounce(
+  (r: MutationRecord[], o: MutationObserver) => {
+    changeTableHandler(r, o)
+  },
+  1000, // 単位: ミリ秒、必要に応じて調整
+  { leading: false, trailing: true }
+)
+
+
+let isHandling = false
 async function changeTableHandler(r: MutationRecord[], o:MutationObserver) {
+  console.log('try update')
+
+  stop_observe(o)
+  if (isHandling) return
+  isHandling = true
+
   if (isChageTable(r)) {
-    stop_observe(o)
     await updateScrollTable()
-    start_observe(o)
   }
+  start_observe(o)
+  isHandling = false
 }
 
 function main() {
   const obs = new MutationObserver((r, o) => {
-    changeTableHandler(r, o)
+    debouncedChangeTableHandler(r, o)
   })
 
   observePageTransition(
     (url: string) => url.startsWith("https://www.17lands.com/card_data"),
-    () => start_observe(obs),
-    () => stop_observe(obs))
+    () => {
+      start_observe(obs)
+    },
+    () => {
+      stop_observe(obs)
+  })
 }
 
 main()
